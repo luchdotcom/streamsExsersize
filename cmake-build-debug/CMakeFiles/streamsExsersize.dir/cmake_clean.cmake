a054862b98ec4de2c88ec4c634ddfc10e509f784
Lachezar <lachezarivanov05@gmail.com>
2020-04-28 15:50:24 +0300

    test strings labs.

---
+++ cmake-build-debug/CMakeFiles/streamsExsersize.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/streamsExsersize.dir/main.cpp.obj"
+  "libstreamsExsersize.dll.a"
+  "streamsExsersize.exe"
+  "streamsExsersize.exe.manifest"
+  "streamsExsersize.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/streamsExsersize.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
